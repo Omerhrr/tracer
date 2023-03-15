da5fdfb6bf572f043e7e881dfee4775341805d18
Omerhrr <pyomerhrr@gmail.com>
2023-03-15 15:30:37 +0100

    massive commit

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,15 @@
 # build_file.sh
 echo "Building new project"
-echo "Installing Requirements"
+
 
 # build_files.sh
+echo "Upgrading pip"
+pip install --upgrade pip
+echo "Installing Django"
+pip install django
+echo "Installing MysqlClient"
+pip install mysqlclient
+echo "Installing Requirements"
 pip install -r requirements.txt
 
 echo "Collect Static..."
